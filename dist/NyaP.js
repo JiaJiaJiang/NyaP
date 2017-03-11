c76174b312a0e20831838a279df32c98d14986d1
luojia <luojia@luojia.me>
2017-03-11 09:46:09 +0800

    no message

--- dist/NyaP.js
+++ dist/NyaP.js
@@ -2481,30 +2481,11 @@ function init(DanmakuFrame, DanmakuFrameModule) {
 		}, {
 			key: 'loadList',
 			value: function loadList(danmakuArray) {
-				var _iteratorNormalCompletion = true;
-				var _didIteratorError = false;
-				var _iteratorError = undefined;
+				var _this3 = this;
 
-				try {
-					for (var _iterator = danmakuArray[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-						var d = _step.value;
-
-						this.load(d);
-					}
-				} catch (err) {
-					_didIteratorError = true;
-					_iteratorError = err;
-				} finally {
-					try {
-						if (!_iteratorNormalCompletion && _iterator.return) {
-							_iterator.return();
-						}
-					} finally {
-						if (_didIteratorError) {
-							throw _iteratorError;
-						}
-					}
-				}
+				danmakuArray.forEach(function (d) {
+					return _this3.load(d);
+				});
 			}
 		}, {
 			key: 'unload',
@@ -2672,64 +2653,23 @@ function init(DanmakuFrame, DanmakuFrameModule) {
 					return;
 				}
 				var ctx = this.COL.context;
-				var _iteratorNormalCompletion2 = true;
-				var _didIteratorError2 = false;
-				var _iteratorError2 = undefined;
-
-				try {
-					for (var _iterator2 = this.COL_DanmakuText[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-						var t = _step2.value;
-
-						if (t.drawn) {
-							ctx.clearRect(t.style.x - t.estimatePadding, t.style.y - t.estimatePadding, t._cache.width, t._cache.height);
-						} else {
-							t.drawn = true;
-						}
+				this.COL_DanmakuText.forEach(function (t) {
+					if (t.drawn) {
+						ctx.clearRect(t.style.x - t.estimatePadding, t.style.y - t.estimatePadding, t._cache.width, t._cache.height);
+					} else {
+						t.drawn = true;
 					}
-				} catch (err) {
-					_didIteratorError2 = true;
-					_iteratorError2 = err;
-				} finally {
-					try {
-						if (!_iteratorNormalCompletion2 && _iterator2.return) {
-							_iterator2.return();
-						}
-					} finally {
-						if (_didIteratorError2) {
-							throw _iteratorError2;
-						}
-					}
-				}
+				});
 			}
 		}, {
 			key: 'clear',
 			value: function clear() {
+				var _this4 = this;
+
 				//clear danmaku on the screen
-				var _iteratorNormalCompletion3 = true;
-				var _didIteratorError3 = false;
-				var _iteratorError3 = undefined;
-
-				try {
-					for (var _iterator3 = this.COL_DanmakuText[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-						var t = _step3.value;
-
-						if (t.danmaku) this.removeText(t);
-					}
-				} catch (err) {
-					_didIteratorError3 = true;
-					_iteratorError3 = err;
-				} finally {
-					try {
-						if (!_iteratorNormalCompletion3 && _iterator3.return) {
-							_iterator3.return();
-						}
-					} finally {
-						if (_didIteratorError3) {
-							throw _iteratorError3;
-						}
-					}
-				}
-
+				this.COL_DanmakuText.forEach(function (t) {
+					if (t.danmaku) _this4.removeText(t);
+				});
 				this.tunnel.reset();
 				this._clearCanvas(true);
 			}
@@ -2755,35 +2695,16 @@ function init(DanmakuFrame, DanmakuFrameModule) {
 		}, {
 			key: 'resetTimeOfDanmakuOnScreen',
 			value: function resetTimeOfDanmakuOnScreen() {
+				var _this5 = this;
+
 				var cTime = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.frame.time;
 
 				//cause the position of the danmaku is based on time
 				//and if you don't want these danmaku on the screen to disappear,their time should be reset
-				var _iteratorNormalCompletion4 = true;
-				var _didIteratorError4 = false;
-				var _iteratorError4 = undefined;
-
-				try {
-					for (var _iterator4 = this.COL_DanmakuText[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
-						var t = _step4.value;
-
-						if (!t.danmaku) continue;
-						t.time = cTime - (this.danmakuMoveTime - t.time);
-					}
-				} catch (err) {
-					_didIteratorError4 = true;
-					_iteratorError4 = err;
-				} finally {
-					try {
-						if (!_iteratorNormalCompletion4 && _iterator4.return) {
-							_iterator4.return();
-						}
-					} finally {
-						if (_didIteratorError4) {
-							throw _iteratorError4;
-						}
-					}
-				}
+				this.COL_DanmakuText.forEach(function (t) {
+					if (!t.danmaku) return;
+					t.time = cTime - (_this5.danmakuMoveTime - t.time);
+				});
 			}
 		}, {
 			key: 'danmakuAt',
@@ -2791,32 +2712,10 @@ function init(DanmakuFrame, DanmakuFrameModule) {
 				//return a list of danmaku which is over this position
 				var list = [];
 				if (!this.enabled) return list;
-				var _iteratorNormalCompletion5 = true;
-				var _didIteratorError5 = false;
-				var _iteratorError5 = undefined;
-
-				try {
-					for (var _iterator5 = this.COL_DanmakuText[Symbol.iterator](), _step5; !(_iteratorNormalCompletion5 = (_step5 = _iterator5.next()).done); _iteratorNormalCompletion5 = true) {
-						var t = _step5.value;
-
-						if (!t.danmaku) continue;
-						if (t.style.x <= x && t.style.x + t.style.width >= x && t.style.y <= y && t.style.y + t.style.height >= y) list.push(t.danmaku);
-					}
-				} catch (err) {
-					_didIteratorError5 = true;
-					_iteratorError5 = err;
-				} finally {
-					try {
-						if (!_iteratorNormalCompletion5 && _iterator5.return) {
-							_iterator5.return();
-						}
-					} finally {
-						if (_didIteratorError5) {
-							throw _iteratorError5;
-						}
-					}
-				}
-
+				this.COL_DanmakuText.forEach(function (t) {
+					if (!t.danmaku) return;
+					if (t.style.x <= x && t.style.x + t.style.width >= x && t.style.y <= y && t.style.y + t.style.height >= y) list.push(t.danmaku);
+				});
 				return list;
 			}
 		}, {

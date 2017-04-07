0e045a4dbd126ec725ad7110869a4591ffa343e8
luojia <luojia@luojia.me>
2017-04-08 00:33:37 +0800

    no message

--- dist/NyaP.js
+++ dist/NyaP.js
@@ -2792,12 +2792,12 @@ var NyaP = function (_NyaPlayerCore) {
 		video.controls = false;
 		var icons = {
 			play: [30, 30, '<path d="m10.063,8.856l9.873,6.143l-9.873,6.143v-12.287z" stroke-width="3" stroke-linejoin="round"/>'],
-			addDanmaku: [30, 30, '<path stroke-width="0.9" d="m21.004,8.995c-0.513,-0.513 -1.135,-0.770 -1.864,-0.770l-8.281,0c-0.729,0 -1.350,0.256 -1.864,0.770c-0.513,0.513 -0.770,1.135 -0.770,1.864l0,8.281c0,0.721 0.256,1.341 0.770,1.858c0.513,0.517 1.135,0.776 1.864,0.776l8.281,0c0.729,0 1.350,-0.258 1.864,-0.776c0.513,-0.517 0.770,-1.136 0.770,-1.858l0,-8.281c0,-0.729 -0.257,-1.350 -0.770,-1.864zm-0.734,10.145c0,0.313 -0.110,0.580 -0.329,0.799c-0.219,0.219 -0.486,0.329 -0.799,0.329l-8.281,0c-0.313,0 -0.580,-0.109 -0.799,-0.329c-0.219,-0.219 -0.329,-0.486 -0.329,-0.799l0,-8.281c0,-0.313 0.109,-0.580 0.329,-0.799c0.219,-0.219 0.486,-0.329 0.799,-0.329l8.281,0c0.313,0 0.580,0.109 0.799,0.329c0.219,0.219 0.329,0.486 0.329,0.799l0,8.281zm-1.882,-4.893l-2.634,0l0,-2.634c0,-0.109 -0.035,-0.199 -0.106,-0.270c-0.070,-0.070 -0.160,-0.105 -0.270,-0.105l-0.752,0c-0.109,0 -0.199,0.035 -0.270,0.105c-0.070,0.070 -0.105,0.160 -0.105,0.270l0,2.634l-2.634,0c-0.109,0 -0.200,0.035 -0.270,0.105c-0.070,0.070 -0.105,0.160 -0.105,0.270l0,0.752c0,0.109 0.035,0.200 0.105,0.270c0.070,0.070 0.160,0.105 0.270,0.105l2.634,0l0,2.634c0,0.109 0.035,0.200 0.105,0.270c0.070,0.070 0.160,0.105 0.270,0.105l0.752,0c0.110,0 0.200,-0.035 0.270,-0.105c0.070,-0.070 0.106,-0.160 0.106,-0.270l0,-2.634l2.634,0c0.109,0 0.199,-0.035 0.270,-0.105c0.070,-0.070 0.105,-0.160 0.105,-0.270l0,-0.752c0,-0.109 -0.035,-0.200 -0.105,-0.270c-0.070,-0.070 -0.160,-0.105 -0.270,-0.105z" stroke-linejoin="round"/>'],
+			addDanmaku: [30, 30, '<path style="fill-opacity:0!important;" stroke-width="1.4" d="m21.004,8.995c-0.513,-0.513 -1.135,-0.770 -1.864,-0.770l-8.281,0c-0.729,0 -1.350,0.256 -1.864,0.770c-0.513,0.513 -0.770,1.135 -0.770,1.864l0,8.281c0,0.721 0.256,1.341 0.770,1.858c0.513,0.517 1.135,0.776 1.864,0.776l8.281,0c0.729,0 1.350,-0.258 1.864,-0.776c0.513,-0.517 0.770,-1.136 0.770,-1.858l0,-8.281c0,-0.729 -0.257,-1.350 -0.770,-1.864z" stroke-linejoin="round"/>' + '<path d="m12.142,14.031l1.888,0l0,-1.888l1.937,0l0,1.888l1.888,0l0,1.937l-1.888,0l0,1.888l-1.937,0l0,-1.888l-1.888,0l0,-1.937z" stroke-width="1"/>'],
 			danmakuStyle: [30, 30, '<path style="fill-opacity:1!important" d="m21.781,9.872l-1.500,-1.530c-0.378,-0.385 -0.997,-0.391 -1.384,-0.012l-0.959,0.941l2.870,2.926l0.960,-0.940c0.385,-0.379 0.392,-0.998 0.013,-1.383zm-12.134,7.532l2.871,2.926l7.593,-7.448l-2.872,-2.927l-7.591,7.449l0.000,0.000zm-1.158,2.571l-0.549,1.974l1.984,-0.511l1.843,-0.474l-2.769,-2.824l-0.509,1.835z" stroke-width="0"/>'],
 			fullPage: [30, 30, '<path stroke-linejoin="round" d="m11.166,9.761l-5.237,5.239l5.237,5.238l1.905,-1.905l-3.333,-3.333l3.332,-3.333l-1.904,-1.906zm7.665,0l-1.903,1.905l3.332,3.333l-3.332,3.332l1.903,1.905l5.238,-5.238l-5.238,-5.237z" stroke-width="1.3" />'],
 			fullScreen: [30, 30, '<rect stroke-linejoin="round" height="11.169" width="17.655" y="9.415" x="6.172" stroke-width="1.5"/>' + '<path stroke-linejoin="round" d="m12.361,11.394l-3.604,3.605l3.605,3.605l1.311,-1.311l-2.294,-2.294l2.293,-2.294l-1.311,-1.311zm5.275,0l-1.310,1.311l2.293,2.294l-2.293,2.293l1.310,1.311l3.605,-3.605l-3.605,-3.605z"/>'],
 			loop: [30, 30, '<path stroke-linejoin="round" stroke-width="1" d="m20.945,15.282c-0.204,-0.245 -0.504,-0.387 -0.823,-0.387c-0.583,0 -1.079,0.398 -1.205,0.969c-0.400,1.799 -2.027,3.106 -3.870,3.106c-2.188,0 -3.969,-1.780 -3.969,-3.969c0,-2.189 1.781,-3.969 3.969,-3.969c0.720,0 1.412,0.192 2.024,0.561l-0.334,0.338c-0.098,0.100 -0.127,0.250 -0.073,0.380c0.055,0.130 0.183,0.213 0.324,0.212l2.176,0.001c0.255,-0.002 0.467,-0.231 0.466,-0.482l-0.008,-2.183c-0.000,-0.144 -0.085,-0.272 -0.217,-0.325c-0.131,-0.052 -0.280,-0.022 -0.379,0.077l-0.329,0.334c-1.058,-0.765 -2.340,-1.182 -3.649,-1.182c-3.438,0 -6.236,2.797 -6.236,6.236c0,3.438 2.797,6.236 6.236,6.236c2.993,0 5.569,-2.133 6.126,-5.072c0.059,-0.314 -0.022,-0.635 -0.227,-0.882z"/>'],
-			volume: [30, 30, '<ellipse id="volume_circle" style="fill-opacity:.6!important" ry="5" rx="5" cy="15" cx="15" stroke-dasharray="32 90" stroke-width="1.8"/>'],
+			volume: [30, 30, '<ellipse id="volume_circle" style="fill-opacity:.6!important" ry="6" rx="6" cy="15" cx="15" stroke-dasharray="38 90" stroke-width="1.8"/>'],
 			danmakuMode0: [30, 30, '<path style="fill-opacity:1!important" stroke-width="0" d="m14.981,17.821l-7.937,-2.821l7.937,-2.821l0,1.409l7.975,0l0,2.821l-7.975,0l0,1.409l0,0.002z"/>'],
 			danmakuMode1: [30, 30, '<path style="fill-opacity:1!important" stroke-width="0" d="m15.019,12.178l7.937,2.821l-7.937,2.821l0,-1.409l-7.975,0l0,-2.821l7.975,0l0,-1.409l0,-0.002z"/>'],
 			danmakuMode3: [30, 30, '<path stroke-width="3" d="m7.972,7.486l14.054,0"/>'],
@@ -2900,7 +2900,7 @@ var NyaP = function (_NyaPlayerCore) {
 					_this._setTimeInfo(null, (0, _NyaPCore.formatTime)(video.duration, video.duration));
 				},
 				volumechange: function volumechange(e) {
-					(0, _NyaPCore.setAttrs)($.volume_circle, { 'stroke-dasharray': video.volume * 10 * Math.PI + ' 90', style: 'fill-opacity:' + (video.muted ? .2 : .6) + '!important' });
+					(0, _NyaPCore.setAttrs)($.volume_circle, { 'stroke-dasharray': video.volume * 12 * Math.PI + ' 90', style: 'fill-opacity:' + (video.muted ? .2 : .6) + '!important' });
 					$.icon_span_volume.setAttribute('title', _('volume($0)([shift]+↑↓)', video.muted ? _('muted') : (video.volume * 100 | 0) + '%'));
 				},
 				progress: function progress(e) {
@@ -3121,7 +3121,9 @@ var NyaP = function (_NyaPlayerCore) {
 			var $ = this.$;
 			$.danmaku_input_frame.style.display = bool ? 'flex' : '';
 			$.icon_span_addDanmaku.classList[bool ? 'add' : 'remove']('active_icon');
-			bool ? $.danmaku_input.focus() : $.playerKeyEvent.focus();
+			setImmediate(function () {
+				bool ? $.danmaku_input.focus() : $.playerKeyEvent.focus();
+			});
 		}
 	}, {
 		key: 'playerMode',
@@ -3200,16 +3202,6 @@ var NyaP = function (_NyaPlayerCore) {
 						_this3.danmakuInput(false);
 					}
 				});
-			}
-		}
-	}, {
-		key: 'menu',
-		value: function menu(position) {
-			console.log('position', position);
-			if (position) {
-				//if position is defined,find out the danmaku at that position and enable danmaku oprion in menu
-				var ds = this.danmakuFrame.modules.TextDanmaku.danmakuAt(position[0], position[1]);
-				console.log(ds);
 			}
 		}
 	}, {

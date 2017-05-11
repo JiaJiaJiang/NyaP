70048ce4e6edc438595cb9f243d2ce2e59fc291a
luojia <luojia@luojia.me>
2017-05-11 14:19:37 +0800

    Object.assign polyfill

--- dist/NyaP.es2015.js
+++ dist/NyaP.es2015.js
@@ -3486,6 +3486,24 @@ if (!String.prototype.startsWith) String.prototype.startsWith = function (search
 
 	return this.substr(position, searchString.length) === searchString;
 };
+//Polyfill from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+if (!Object.assign) Object.assign = function (target, varArgs) {
+	'use strict';
+
+	if (target == null) throw new TypeError('Cannot convert undefined or null to object');
+	var to = Object(target);
+	for (var index = 1; index < arguments.length; index++) {
+		var nextSource = arguments[index];
+		if (nextSource != null) {
+			for (var nextKey in nextSource) {
+				if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+					to[nextKey] = nextSource[nextKey];
+				}
+			}
+		}
+	}
+	return to;
+};
 
 exports.default = NyaPlayerCore;
 exports.NyaPlayerCore = NyaPlayerCore;
